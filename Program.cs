95dfd54954280b04f13970d4494945246a40fa2e
Levid <jassontejada@gmail.com>
2023-01-29 16:39:30 -0400

    Eliminando clase PrintMyName y haciendola una función dentro del Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Exercises
     {
         public static void Main(string[] args)
         {
-            new PrintMyName();
+            PrintMyName();
+        }
+
+        public static void PrintMyName()
+        {
+            //1 Cree un proyecto nuevo que imprima su nombre en la ventana de la consola.
+            Console.WriteLine("Hola, Levid.");
+            //5 Agregue más mensajes a la aplicación que ha creado.
+            Console.WriteLine("He agregado una linea.");
         }
     }
 }
